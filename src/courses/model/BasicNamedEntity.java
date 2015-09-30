7f738e1f1584878079f4d5a45c9e0dcf04706710
stefankiv <stefankiv@inbox.ru>
2015-09-30 18:58:08 +0300

    added basic class

---
+++ src/courses/model/BasicNamedEntity.java
@@ -0,0 +1,13 @@
+package courses.model;
+
+public abstract class BasicNamedEntity {
+	protected String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
